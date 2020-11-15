f1fe9b2159183aef2971be60af1c4e718e802c62
Balazs Krizsan <krizsan.balazs@gmail.com>
2020-11-15 13:21:26 +0100

    AddressCreateRequest validation update

--- src/main/java/com/kbalazsworks/stackjudge/api/requests/company_request/AddressCreateRequest.java
+++ src/main/java/com/kbalazsworks/stackjudge/api/requests/company_request/AddressCreateRequest.java
@@ -2,22 +2,21 @@ package com.kbalazsworks.stackjudge.api.requests.company_request;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-import java.math.BigDecimal;
 
 public record AddressCreateRequest(
     @Size(min = 10, max = 255, groups = IAddressRequestValidationGroup.class)
     String fullAddress,
 
-    @Pattern(regexp = "^\\d{1,9}\\.\\d{1,20}$")
+    @Pattern(regexp = "^\\d{1,3}\\.\\d{1,20}$")
     Double markerLat,
 
-    @Pattern(regexp = "^\\d{1,9}\\.\\d{1,20}$")
+    @Pattern(regexp = "^\\d{1,3}\\.\\d{1,20}$")
     Double markerLng,
 
-    @Pattern(regexp = "^\\d{1,9}\\.\\d{1,20}$")
+    @Pattern(regexp = "^\\d{1,3}\\.\\d{1,20}$")
     Double manualMarkerLat,
 
-    @Pattern(regexp = "^\\d{1,9}\\.\\d{1,20}$")
+    @Pattern(regexp = "^\\d{1,3}\\.\\d{1,20}$")
     Double manualMarkerLng
 )
 {
