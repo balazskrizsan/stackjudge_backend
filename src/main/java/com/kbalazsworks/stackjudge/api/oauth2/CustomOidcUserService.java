5ce496a56687bfd58428d7a594dbf14fab575424
Balazs Krizsan <krizsan.balazs@gmail.com>
2020-12-14 19:13:34 +0100

    auth try 2

---
+++ src/main/java/com/kbalazsworks/stackjudge/api/oauth2/CustomOidcUserService.java
@@ -0,0 +1,35 @@
+package com.kbalazsworks.stackjudge.api.oauth2;
+
+import com.kbalazsworks.stackjudge.api.exceptions.OAuth2AuthenticationProcessingException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CustomOidcUserService extends OidcUserService {
+
+//	@Autowired
+//	private UserService userService;
+
+	@Override
+	public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
+		OidcUser oidcUser = super.loadUser(userRequest);
+		try {
+			throw new OAuth2AuthenticationProcessingException("juppijjje2");
+//			return userService.processUserRegistration(userRequest.getClientRegistration().getRegistrationId(), oidcUser.getAttributes(), oidcUser.getIdToken(),
+//					oidcUser.getUserInfo());
+		} catch (AuthenticationException ex) {
+			throw ex;
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			// Throwing an instance of AuthenticationException will trigger the
+			// OAuth2AuthenticationFailureHandler
+			throw new OAuth2AuthenticationProcessingException(ex.getMessage(), ex.getCause());
+		}
+	}
+}
