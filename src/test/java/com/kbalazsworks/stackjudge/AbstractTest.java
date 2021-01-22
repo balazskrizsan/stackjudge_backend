b5167796c1a4f55d4081ca91f1d054a07872cdd2
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-01-22 14:01:16 +0100

    AbstractTest annotation update

--- src/test/java/com/kbalazsworks/stackjudge/AbstractTest.java
+++ src/test/java/com/kbalazsworks/stackjudge/AbstractTest.java
@@ -2,11 +2,13 @@ package com.kbalazsworks.stackjudge;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:application.properties")
+@ContextConfiguration(classes = StackJudgeApplication.class)
 public abstract class AbstractTest {
 }
