56272ca00f9e893d4d968605fbeaf00ccc2c0f89
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-04-29 00:49:57 +0200

    RegistrationAction refact

--- src/main/java/com/kbalazsworks/stackjudge/api/controllers/account_controller/RegistrationAction.java
+++ src/main/java/com/kbalazsworks/stackjudge/api/controllers/account_controller/RegistrationAction.java
@@ -4,11 +4,10 @@ import com.github.scribejava.apis.FacebookApi;
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.oauth.OAuth20Service;
 import com.kbalazsworks.stackjudge.api.builders.ResponseEntityBuilder;
-import com.kbalazsworks.stackjudge.api.value_objects.ResponseData;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.Random;
 
@@ -17,19 +16,16 @@ import java.util.Random;
 public class RegistrationAction
 {
     @GetMapping("/registration")
-    public ResponseEntity<ResponseData<String>> action() throws Exception
+    public RedirectView action() throws Exception
     {
-        final String clientId     = "149500276863432"; //@todo: remove and change
-        final String clientSecret = "4738a5809b56a5a2087fb923958fdd4d"; //@todo: remove and change
-        final OAuth20Service service = new ServiceBuilder(clientId)
+        String clientId     = "149500276863432"; //@todo: remove and change
+        String clientSecret = "4738a5809b56a5a2087fb923958fdd4d"; //@todo: remove and change
+
+        OAuth20Service service = new ServiceBuilder(clientId)
             .apiSecret(clientSecret)
             .callback("https://localhost:8181/account/facebook-callback")
             .build(FacebookApi.instance());
 
-        final String secretState      = "secret" + new Random().nextInt(999_999);
-        final String authorizationUrl = service.getAuthorizationUrl(secretState);
-
-        // redir to
-        return new ResponseEntityBuilder<String>().data(authorizationUrl).build();
+        return new RedirectView(service.getAuthorizationUrl("secret" + new Random().nextInt(999_999)));
     }
 }
