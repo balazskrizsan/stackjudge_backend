510552112b2659c53e064a78166f73b3e85fce6e
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-05-25 21:49:05 +0200

    JwtService refact

--- src/main/java/com/kbalazsworks/stackjudge/api/services/JwtService.java
+++ src/main/java/com/kbalazsworks/stackjudge/api/services/JwtService.java
@@ -1,8 +1,10 @@
 package com.kbalazsworks.stackjudge.api.services;
 
+import com.kbalazsworks.stackjudge.spring_config.ApplicationProperties;
 import com.kbalazsworks.stackjudge.state.entities.User;
 import io.jsonwebtoken.*;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -10,65 +12,82 @@ import java.util.Date;
 
 import static java.lang.String.format;
 
-@Slf4j
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class JwtService
 {
-    private final String jwtSecret = "zdtlD3JK56m6wTTgsNFhqzjqP"; //@todo: remove and change
-    private final String jwtIssuer = "example.io"; //@todo: remove and change
+    private final ApplicationProperties applicationProperties;
 
     public String generateAccessToken(@NonNull User user)
     {
-        return Jwts.builder()
-                   .setSubject(format("%s,%s", user.getId(), user.getUsername()))
-                   .setIssuer(jwtIssuer)
-                   .setIssuedAt(new Date())
-                   .setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 1 week
-                   .signWith(SignatureAlgorithm.HS512, jwtSecret)
-                   .compact();
+        return Jwts
+            .builder()
+            .setSubject(format("%s,%s", user.getId(), user.getUsername()))
+            .setIssuer(applicationProperties.getSiteDomain())
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 7 * 24 * 60 * 60 * 1000)) // 1 week
+            .signWith(SignatureAlgorithm.HS512, applicationProperties.getJwtSecret())
+            .compact();
     }
 
-    public String getUserId(String token)
+    public Long getUserId(String token)
     {
-        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        Claims claims = Jwts.parser()
+            .setSigningKey(applicationProperties.getJwtSecret())
+            .parseClaimsJws(token)
+            .getBody();
 
-        return claims.getSubject().split(",")[0];
+        return Long.valueOf(claims.getSubject().split(",")[0]);
     }
 
     public String getUsername(String token)
     {
-        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        Claims claims = Jwts
+            .parser()
+            .setSigningKey(applicationProperties.getJwtSecret())
+            .parseClaimsJws(token)
+            .getBody();
 
         return claims.getSubject().split(",")[1];
     }
 
     public Date getExpirationDate(String token)
     {
-        Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
+        Claims claims = Jwts
+            .parser()
+            .setSigningKey(applicationProperties.getJwtSecret())
+            .parseClaimsJws(token)
+            .getBody();
 
         return claims.getExpiration();
     }
 
-    public boolean validate(String token)
+    public boolean isValid(@NonNull String token)
     {
         try
         {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            Jwts.parser().setSigningKey(applicationProperties.getJwtSecret()).parseClaimsJws(token);
 
             return true;
-        } catch (SignatureException ex)
+        }
+        catch (SignatureException ex)
         {
             log.error("Invalid JWT signature - {}", ex.getMessage());
-        } catch (MalformedJwtException ex)
+        }
+        catch (MalformedJwtException ex)
         {
             log.error("Invalid JWT token - {}", ex.getMessage());
-        } catch (ExpiredJwtException ex)
+        }
+        catch (ExpiredJwtException ex)
         {
             log.error("Expired JWT token - {}", ex.getMessage());
-        } catch (UnsupportedJwtException ex)
+        }
+        catch (UnsupportedJwtException ex)
         {
             log.error("Unsupported JWT token - {}", ex.getMessage());
-        } catch (IllegalArgumentException ex)
+        }
+        catch (IllegalArgumentException ex)
         {
             log.error("JWT claims string is empty - {}", ex.getMessage());
         }
