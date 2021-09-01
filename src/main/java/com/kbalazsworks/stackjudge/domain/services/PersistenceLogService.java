8073424c5768b8cf5fc3dc67093f37675c7c9e90
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-09-01 18:46:56 +0200

    PersistenceLogService will never throw exception

--- src/main/java/com/kbalazsworks/stackjudge/domain/services/PersistenceLogService.java
+++ src/main/java/com/kbalazsworks/stackjudge/domain/services/PersistenceLogService.java
@@ -4,9 +4,11 @@ import com.kbalazsworks.stackjudge.domain.entities.TypedPersistenceLog;
 import com.kbalazsworks.stackjudge.domain.repositories.PersistenceLogRepository;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
+@Log4j2
 @RequiredArgsConstructor
 public class PersistenceLogService
 {
@@ -14,6 +16,13 @@ public class PersistenceLogService
 
     public <T> void create(@NonNull TypedPersistenceLog<T> typedPersistenceLog)
     {
-        persistenceLogRepository.create(typedPersistenceLog);
+        try
+        {
+            persistenceLogRepository.create(typedPersistenceLog);
+        }
+        catch (Exception e)
+        {
+            log.error("PersistenceLogService error: " + e.getMessage());
+        }
     }
 }
