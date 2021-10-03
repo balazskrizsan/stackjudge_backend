1b2b2ffcdf4769dc0be85acd220d1726e6de3ef2
Balazs Krizsan <krizsan.balazs@gmail.com>
2021-10-03 02:37:01 +0200

    RegistrationStateService remove token after 24h

--- src/main/java/com/kbalazsworks/stackjudge/domain/services/RegistrationStateService.java
+++ src/main/java/com/kbalazsworks/stackjudge/domain/services/RegistrationStateService.java
@@ -3,18 +3,23 @@ package com.kbalazsworks.stackjudge.domain.services;
 import com.kbalazsworks.stackjudge.domain.entities.RegistrationSecret;
 import com.kbalazsworks.stackjudge.domain.repositories.RegistrationSecretRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 public class RegistrationStateService
 {
-    private final RegistrationSecretRepository registrationSecretRepository;
+    private final RegistrationSecretRepository              registrationSecretRepository;
+    private final RedisTemplate<String, RegistrationSecret> redisTemplate;
 
-    // @todo2: test
+    // @todo: test
     public void add(String state)
     {
         registrationSecretRepository.save(new RegistrationSecret(state, state));
+        redisTemplate.expire(state, 24, TimeUnit.HOURS);
     }
 
     // @todo2: test
