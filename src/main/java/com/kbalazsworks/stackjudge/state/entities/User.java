7089f03004e42d424219e5d4a61964bd35d817b5
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-01-13 16:08:56 +0100

    CS

--- src/main/java/com/kbalazsworks/stackjudge/state/entities/User.java
+++ src/main/java/com/kbalazsworks/stackjudge/state/entities/User.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 
 @Entity
