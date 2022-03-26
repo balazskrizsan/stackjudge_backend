d07dbe5daef057e672d9058f0def6b3cadd011c1
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-03-26 15:57:16 +0100

    User entity updates

--- src/main/java/com/kbalazsworks/stackjudge/state/entities/User.java
+++ src/main/java/com/kbalazsworks/stackjudge/state/entities/User.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -17,19 +18,25 @@ import java.io.Serializable;
 @Getter(onMethod = @__(@JsonIgnore))
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class User implements Serializable
 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Getter
     private Long    id;
+    @Getter
     private Boolean isEmailUser;
+    @Getter
     private Boolean isFacebookUser;
     @Getter
     private String  profilePictureUrl;
     @Getter
     private String  username;
+    @ToString.Exclude
     private String  password;
+    @ToString.Exclude
     private String  facebookAccessToken;
+    @Getter
     private Long    facebookId;
 }
