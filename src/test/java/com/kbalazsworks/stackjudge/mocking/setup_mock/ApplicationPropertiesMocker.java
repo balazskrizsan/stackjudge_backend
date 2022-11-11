8decf9304feb22932af05ae62c3a58b4dff0b817
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-11-12 00:46:39 +0100

    ApplicationPropertiesMocker cleanup

--- src/test/java/com/kbalazsworks/stackjudge/mocking/setup_mock/ApplicationPropertiesMocker.java
+++ src/test/java/com/kbalazsworks/stackjudge/mocking/setup_mock/ApplicationPropertiesMocker.java
@@ -2,20 +2,17 @@ package com.kbalazsworks.stackjudge.mocking.setup_mock;
 
 import com.kbalazsworks.stackjudge.mocking.MockCreator;
 import com.kbalazsworks.stackjudge.spring_config.ApplicationProperties;
+import lombok.NonNull;
 
 import static org.mockito.Mockito.when;
 
 public class ApplicationPropertiesMocker extends MockCreator
 {
-    public static final String AWS_ACCESS_KEY     = "aws-access-key";
-    public static final String AWS_SECRET_KEY     = "aws-secret-key";
-    public static final String AWS_S3_CDN_BUCKET  = "aws-s3-cdn-bucket";
     public static final String GOOGLE_MAPS_KEY    = "aws-s3-cdn-bucket";
-    public static final String JWT_SECRET         = "12345678901234567890123456789012";
     public static final String SITE_DOMAIN        = "dev.stackjudge.com";
     public static final String SITE_FRONTEND_HOST = "http://stackjudge.com";
 
-    public static ApplicationProperties getDefaultMock()
+    public static @NonNull ApplicationProperties getDefaultMock()
     {
         ApplicationProperties mock = getApplicationPropertiesMock();
 
