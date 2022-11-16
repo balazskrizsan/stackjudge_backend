1883775981bec37cba90275937a91ef9d0695b2e
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-11-16 22:17:13 +0100

    SecureRandomService_getUrlEncodedTest refact

--- src/test/java/com/kbalazsworks/stackjudge/unit/common/services/SecureRandomService_getUrlEncodedTest.java
+++ src/test/java/com/kbalazsworks/stackjudge/unit/common/services/SecureRandomService_getUrlEncodedTest.java
@@ -3,11 +3,15 @@ package com.kbalazsworks.stackjudge.unit.common.services;
 import com.kbalazsworks.stackjudge.AbstractTest;
 import com.kbalazsworks.stackjudge.ServiceFactory;
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Base64;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -23,41 +27,29 @@ public class SecureRandomService_getUrlEncodedTest extends AbstractTest
         assertTrue(true);
     }
 
-    private record TestData(int testedLength, int expectedLength)
+    static class Provider_testingWithDifferentLength_checkedByProvider implements ArgumentsProvider
     {
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context)
+        {
+            return Stream.of(
+                Arguments.of(0, 0),
+                Arguments.of(20, 20),
+                Arguments.of(1000, 1000)
+            );
+        }
     }
 
-    private TestData provider(int repetition)
-    {
-        if (1 == repetition)
-        {
-            return new TestData(0, 0);
-        }
-
-        if (2 == repetition)
-        {
-            return new TestData(20, 20);
-        }
-
-        if (3 == repetition)
-        {
-            return new TestData(1000, 1000);
-        }
-
-        throw getRepeatException(repetition);
-    }
-
-    @RepeatedTest(3)
-    public void testingWithDifferentLength_perfect(RepetitionInfo repetitionInfo)
+    @ParameterizedTest(name = "{index} => testedLength={0}, expectedLength={1}")
+    @ArgumentsSource(Provider_testingWithDifferentLength_checkedByProvider.class)
+    public void testingWithDifferentLength_checkedByProvider(int testedLength, int expectedLength)
     {
         // Arrange
-        TestData testData = provider(repetitionInfo.getCurrentRepetition());
-
         // Act
-        String actualEncodedRandom = serviceFactory.getSecureRandomService().getUrlEncoded(testData.testedLength);
-        String actualRandom = new String(Base64.getDecoder().decode(actualEncodedRandom.getBytes()));
+        String actualEncodedRandom = serviceFactory.getSecureRandomService().getUrlEncoded(testedLength);
+        String actualRandom        = new String(Base64.getDecoder().decode(actualEncodedRandom.getBytes()));
 
         // Assert
-        assertThat(actualRandom).hasSize(testData.expectedLength);
+        assertThat(actualRandom).hasSize(expectedLength);
     }
 }
