fac45ca0cddf9926ce9738c8c62913a517329a7e
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-11-17 00:16:40 +0100

    SearchMyNotificationsService_hasNewTest refact

--- src/test/java/com/kbalazsworks/stackjudge/unit/domain/notification_module/services/SearchMyNotificationsService_hasNewTest.java
+++ src/test/java/com/kbalazsworks/stackjudge/unit/domain/notification_module/services/SearchMyNotificationsService_hasNewTest.java
@@ -5,12 +5,15 @@ import com.kbalazsworks.stackjudge.ServiceFactory;
 import com.kbalazsworks.stackjudge.domain.notification_module.entities.ITypedNotification;
 import com.kbalazsworks.stackjudge.fake_builders.TypedNotificationFakeBuilder;
 import org.junit.Test;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.RepetitionInfo;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,49 +28,39 @@ public class SearchMyNotificationsService_hasNewTest extends AbstractTest
         assertThat(true).isTrue();
     }
 
-    private record TestData(List<ITypedNotification> testedNotifications, boolean expectedResult)
+    static class Provider_listHasNewItem_returnsTrue implements ArgumentsProvider
     {
-    }
-
-    private TestData provider(int repetition)
-    {
-        if (1 == repetition)
+        @Override
+        public Stream<? extends Arguments> provideArguments(ExtensionContext context)
         {
-            return new TestData(
-                new ArrayList<>()
-                {{
-                    add(new TypedNotificationFakeBuilder<>().build());
-                    add(new TypedNotificationFakeBuilder<>().viewedAt(null).build());
-                }},
-                true
+            return Stream.of(
+                Arguments.of(
+                    List.of(
+                        new TypedNotificationFakeBuilder<>().build(),
+                        new TypedNotificationFakeBuilder<>().viewedAt(null).build()
+                    ),
+                    true
+                ),
+                Arguments.of(
+                    List.of(
+                        new TypedNotificationFakeBuilder<>().build(),
+                        new TypedNotificationFakeBuilder<>().build()
+                    ),
+                    false
+                )
             );
         }
-
-        if (2 == repetition)
-        {
-            return new TestData(
-                new ArrayList<>()
-                {{
-                    add(new TypedNotificationFakeBuilder<>().build());
-                    add(new TypedNotificationFakeBuilder<>().build());
-                }},
-                false
-            );
-        }
-
-        throw getRepeatException(repetition);
     }
 
-    @RepeatedTest(2)
-    public void listHasNewItem_returnsTrue(RepetitionInfo repetitionInfo)
+    @ParameterizedTest(name = "{index} => testedNotifications={0}")
+    @ArgumentsSource(Provider_listHasNewItem_returnsTrue.class)
+    public void listHasNewItem_returnsTrue(List<ITypedNotification> testedNotifications, boolean expectedResult)
     {
         // Arrange
-        TestData testData = provider(repetitionInfo.getCurrentRepetition());
-
         // Act
-        boolean actual = serviceFactory.getSearchMyNotificationsService().hasNew(testData.testedNotifications());
+        boolean actual = serviceFactory.getSearchMyNotificationsService().hasNew(testedNotifications);
 
         // Assert
-        assertThat(actual).isEqualTo(testData.expectedResult());
+        assertThat(actual).isEqualTo(expectedResult);
     }
 }
