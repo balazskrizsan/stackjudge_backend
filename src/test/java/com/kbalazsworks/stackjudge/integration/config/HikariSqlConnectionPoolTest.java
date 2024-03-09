04594df9f45f6a387ed7a393fdd9fb307a6eb689
Balazs Krizsan <krizsan.balazs@gmail.com>
2024-03-09 22:33:48 +0100

    HikariSqlConnectionPoolTest test fix: @DirtiesContext(methodMode = BEFORE_METHOD)

--- src/test/java/com/kbalazsworks/stackjudge/integration/config/HikariSqlConnectionPoolTest.java
+++ src/test/java/com/kbalazsworks/stackjudge/integration/config/HikariSqlConnectionPoolTest.java
@@ -8,7 +8,9 @@ import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import nl.altindag.log.LogCaptor;
 import org.junit.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.jdbc.SqlGroup;
@@ -21,6 +23,7 @@ import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.test.annotation.DirtiesContext.MethodMode.BEFORE_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.context.jdbc.SqlConfig.TransactionMode.ISOLATED;
@@ -30,6 +33,12 @@ public class HikariSqlConnectionPoolTest extends AbstractIntegrationTest
 {
     @Autowired
     private ServiceFactory serviceFactory;
+
+    @Test
+    public void VintageHack()
+    {
+        assertThat(true).isTrue();
+    }
 
     private record Processor(Function<Integer, Boolean> fn, int i) implements Runnable
     {
@@ -41,24 +50,25 @@ public class HikariSqlConnectionPoolTest extends AbstractIntegrationTest
     }
 
     @SqlGroup(
-      {
-        @Sql(
-          executionPhase = BEFORE_TEST_METHOD,
-          config = @SqlConfig(transactionMode = ISOLATED),
-          scripts = {
-            "classpath:test/sqls/_truncate_tables.sql",
-            "classpath:test/sqls/preset_add_1_company.sql"
-          }
-        ),
-        @Sql(
-          executionPhase = AFTER_TEST_METHOD,
-          config = @SqlConfig(transactionMode = ISOLATED),
-          scripts = {"classpath:test/sqls/_truncate_tables.sql"}
-        )
-      }
+        {
+            @Sql(
+                executionPhase = BEFORE_TEST_METHOD,
+                config = @SqlConfig(transactionMode = ISOLATED),
+                scripts = {
+                    "classpath:test/sqls/_truncate_tables.sql",
+                    "classpath:test/sqls/preset_add_1_company.sql"
+                }
+            ),
+            @Sql(
+                executionPhase = AFTER_TEST_METHOD,
+                config = @SqlConfig(transactionMode = ISOLATED),
+                scripts = {"classpath:test/sqls/_truncate_tables.sql"}
+            )
+        }
     )
-    @Test
+    @RepeatedTest(2)
     @SneakyThrows
+    @DirtiesContext(methodMode = BEFORE_METHOD)
     public void hikariSqlConnectionPool_12insertsOn5threads_creates12recordsOn5connections()
     {
         // Arrange
@@ -67,7 +77,7 @@ public class HikariSqlConnectionPoolTest extends AbstractIntegrationTest
         int testedRuns = 12;
         int expectedLogNumber = 5;
         int expectedAddressRows = 12;
-        String expectedLogMatch = "HikariPool-[\\d] - Added connection org.postgresql.jdbc.PgConnection@[a-z0-9]+";
+        String expectedLogMatch = "HikariPool-[\\d]+ - Added connection org.postgresql.jdbc.PgConnection@[a-z0-9]+";
 
         ExecutorService e = Executors.newFixedThreadPool(testedThreads);
         for (int i = 1; i <= testedRuns; i++)
@@ -88,13 +98,13 @@ public class HikariSqlConnectionPoolTest extends AbstractIntegrationTest
         List<String> logs = logCaptor.getInfoLogs();
 
         assertAll(
-          () -> assertThat(addressCount).isEqualTo(expectedAddressRows),
-          () -> assertThat(logs.size()).isEqualTo(expectedLogNumber),
-          () -> assertThat(logs.get(0)).matches(expectedLogMatch),
-          () -> assertThat(logs.get(1)).matches(expectedLogMatch),
-          () -> assertThat(logs.get(2)).matches(expectedLogMatch),
-          () -> assertThat(logs.get(3)).matches(expectedLogMatch),
-          () -> assertThat(logs.get(4)).matches(expectedLogMatch)
+            () -> assertThat(addressCount).isEqualTo(expectedAddressRows),
+            () -> assertThat(logs.size()).isEqualTo(expectedLogNumber),
+            () -> assertThat(logs.get(0)).matches(expectedLogMatch),
+            () -> assertThat(logs.get(1)).matches(expectedLogMatch),
+            () -> assertThat(logs.get(2)).matches(expectedLogMatch),
+            () -> assertThat(logs.get(3)).matches(expectedLogMatch),
+            () -> assertThat(logs.get(4)).matches(expectedLogMatch)
         );
     }
 }
